2f8845a976001188aac7e6f80b5f928e8059b9c4
Rebecca Neimeyer <rebeccaneimeyer@Rebeccas-MacBook.local>
2022-02-14 15:27:10 -0800

    rendering data by activity type

--- bored/src/Type/Type.js
+++ bored/src/Type/Type.js
@@ -1,24 +1,47 @@
-import './Type.css';
-import {useState, useEffect} from 'react';
+import "./Type.css";
+import { useState, useEffect } from "react";
 
-function Type() {  
+function Type() {
+  const [choice, setChoice] = useState("");
+  const [typeData, setTypeData] = useState({});
 
+  const handleChoice = (event) => {
+    setChoice(event.target.value);
+  };
+  const displayChoice = (event) => {
+    event.preventDefault();
+    fetch(`http://www.boredapi.com/api/activity?type=${choice}`)
+      .then((response) => response.json())
+      .then((data) => setTypeData(data));
+  };
   return (
-      <div className="customized">
-        <label>Activity Type</label>
-        <select name="type" id="type">
-        <option value="" disabled selected hidden>Select</option>
-          <option value="education">Education</option>
-          <option value="recreational">Recreational</option>
-          <option value="social">Social</option>
-          <option value="diy">DIY</option>
-          <option value="charity">Charity</option>
-          <option value="cooking">Cooking</option>
-          <option value="relaxation">Relaxation</option>
-          <option value="music">Music</option>
-          <option value="busywork">Busywork</option>
-        </select>
+    <div className="customized">
+      <form onSubmit={displayChoice}>
+      <label>Activity Type</label>
+      <select name="type" id="type" defaultValue={""} onChange={handleChoice}>
+        <option value="" disabled>
+          Select
+        </option>
+        <option value="education">Education</option>
+        <option value="recreational">Recreational</option>
+        <option value="social">Social</option>
+        <option value="diy">DIY</option>
+        <option value="charity">Charity</option>
+        <option value="cooking">Cooking</option>
+        <option value="relaxation">Relaxation</option>
+        <option value="music">Music</option>
+        <option value="busywork">Busywork</option>
+      </select>
+      <input type="submit" value="See your options!" />
+      </form>
+      <div>
+        <p>Activity: {typeData.activity}</p>
+        <p>Type: {typeData.type}</p>
+        <p>Participants: {typeData.participants}</p>
+        <p>Price: {typeData.price}</p>
+        <p>Accessibility: {typeData.accessibility}</p>
       </div>
+    </div>
   );
 }
 
